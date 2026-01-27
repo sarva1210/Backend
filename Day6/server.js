28e187ea15313e31995aa32114ad73641abb1bb1
sarva1210 <sarva.vish77@gmail.com>
2026-01-27 21:59:27 +0530

    Day6

---
+++ Day6/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+
+const mongoose = require("mongoose")
+const app = require("./src/app")
+
+mongoose.connect(process.env.DB_URI)
+  .then(() => {
+    console.log("Connected to Database")
+
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000")
+    })
+  })
+  .catch((err) => {
+    console.error("DB connection failed:", err)
+  })
