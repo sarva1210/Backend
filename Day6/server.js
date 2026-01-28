a6845344bcbf95f9ddf43a425e984854dc63563b
sarva1210 <sarva.vish77@gmail.com>
2026-01-28 21:51:13 +0530

    changes

--- Day6/server.js
+++ Day6/server.js
@@ -12,6 +12,7 @@ mongoose.connect(process.env.DB_URI)
     app.listen(3000, () => {
       console.log("Server is running on port 3000")
     })
+    
   })
   .catch((err) => {
     console.error("DB connection failed:", err)
