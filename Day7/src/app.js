1aad6b5ffbd1877b4e79a22a142da1e571f49a20
sarva1210 <sarva.vish77@gmail.com>
2026-01-29 22:02:30 +0530

    app.js

---
+++ Day7/src/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+const app = express()
+const noteModel = require('./models/notes.model')
+app.use(express.json())
+
+// post //
+app.post("/notes", async(req, res) => {
+    const { title, description } = req.body
+
+    const note = await noteModel.create({
+        title,description
+    })
+
+    res.status(201).json({
+        message:"Note created successfully",
+        note
+    })
+})
+
+// get //
+app.get("/notes", async (req, res) => {
+    const notes = await noteModel.find()
+ 
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        notes
+    })
+})
+
+
+
+module.exports = app
