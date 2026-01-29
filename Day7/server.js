49fc4dbf61d910fa39836cca67d99b96b0ce0f1f
sarva1210 <sarva.vish77@gmail.com>
2026-01-29 22:02:22 +0530

    server.js

---
+++ Day7/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const connectToDb = require('./src/config/database')
+const app = require('./src/app')
+
+connectToDb()
+
+app.listen(3000,()=>{
+    console.log("server is running in 3000")
+})
