c6d22d9c398acd9ea7da431c2ec20f7b4aacf89d
sarva1210 <sarva.vish77@gmail.com>
2026-01-31 22:39:21 +0530

    js files

---
+++ Day8/Backend/src/app.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const noteModel = require('./models/note.model')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+//  post / api /notes 
+app.post('/api/notes',async(req,res)=>{
+    const{title, description} = req.body
+
+    const note = await noteModel.create({
+        title, description
+    })
+
+    res.status(201).json({
+        message:"note created successfully",
+        note
+    })
+})
+
+
+//  get / api /notes 
+app.get('/api/notes',async(req,res)=>{
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message:"note fetched successfully",
+        notes
+    })
+
+})
+
+//  dlt / api /notes/:id
+app.delete('/api/notes/:id',async(req,res)=>{
+    const id = req.params.id
+
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message:"notes deleted successfully"
+    })
+})
+
+//  patch / api /notes/:id
+app.patch('/api/notes/:id',async(req,res)=>{
+    const id = req.params.id
+    const{description} = req.body
+
+    await noteModel.findByIdAndUpdate(id, {description})
+
+    res.status(200).json({
+        message:"notes updated successfully"
+    })
+})
+
+module.exports = app
