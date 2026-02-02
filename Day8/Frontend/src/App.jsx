8fb03a0119c66d9f1759aed5b0e61aed0bd076b7
sarva1210 <sarva.vish77@gmail.com>
2026-02-02 22:56:28 +0530

    jsx file

--- Day8/Frontend/src/App.jsx
+++ Day8/Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from "axios"
 
 
@@ -11,32 +11,77 @@ function App() {
     // {
     //   title:"test title",
     //   description:"test description"
-    // },
-    // {
-    //   title:"test title",
-    //   description:"test description"
-    // },
-    // {
-    //   title:"test title",
-    //   description:"test description"
     // }
   ])
 
+  console.log("hello integration")
 
-  axios.get('http://localhost:3000/api/notes')
-  .then((res)=>{
-    setNotes(res.data.notes)
+  function fetchNotes(){
+    axios.get('http://localhost:3000/api/notes')
+      .then((res)=>{
+        setNotes(res.data.notes)
+    })
+  }
+
+  useEffect(()=>{
+    fetchNotes()
+  },[])
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    const {title, description} = e.target.elements
+
+    console.log(title.value, description.value)
+
+    axios.post("http://localhost:3000/api/notes",{
+      title:title.value,
+      description:description.value
+    })
+    .then(res=>{
+      console.log(res.data)
+
+      fetchNotes()
+    })
+  }
+
+  function handleDeleteNote(noteId){
+    axios.delete("http://localhost:3000/api/notes/" + noteId)
+    .then(res=>{
+      console.log(res.data)
+      fetchNotes()
+    })
+  }
+
+  function handleUpdateNote(noteId, newDescription) {
+    axios.patch(`http://localhost:3000/api/notes/${noteId}`, {
+    description: newDescription
   })
+  .then(res => {
+    console.log(res.data)
+    fetchNotes()
+  })
+}
 
 
   return (
     <>
+    <form className='note-create-form' onSubmit={handleSubmit}>
+      <input name='title' type="text" placeholder='Enter title'/>
+      <input name='description' type="text" placeholder='Enter description'/>
+      <button>Create Note</button>
+    </form>
     <div className="notes">
       {
         notes.map(note =>{
           return <div className="note">
             <h1>{note.title}</h1>
             <p>{note.description}</p>
+            <button className='btn' onClick={()=>{ 
+              const newDesc = prompt("Enter new description", note.description)
+              if(newDesc){handleUpdateNote(note._id, newDesc)}
+            }} >Edit</button>
+            <button className='btn' onClick={()=>{handleDeleteNote(note._id)}} >Delete</button>
             </div>
         })
       }
