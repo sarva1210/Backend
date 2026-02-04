3790d0006bbc198220112d333a699818f9dd3918
sarva1210 <sarva.vish77@gmail.com>
2026-02-04 20:54:27 +0530

    js file

--- Day7/src/app.js
+++ Day7/src/app.js
@@ -18,15 +18,8 @@ app.post("/notes", async(req, res) => {
     })
 })
 
-// get //
-app.get("/notes", async (req, res) => {
-    const notes = await noteModel.find()
- 
-    res.status(200).json({
-        message: "Notes fetched successfully",
-        notes
-    })
-})
+
+
 
 
 module.exports = app
