cc484a2387469921cf59f3634246b17f258af847
sarva1210 <sarva.vish77@gmail.com>
2026-02-05 21:57:19 +0530

    server js file

---
+++ Day9/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const mongoose = require("mongoose")
+const app = require("./src/app")
+const connectToDb = require("./src/config/database")
+connectToDb()
+
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+})
