5e7c903379db164a70051c3af0dffe64571db7a4
sarva1210 <sarva.vish77@gmail.com>
2026-02-11 22:19:56 +0530

    auth controller

--- project1/src/controllers/auth.controller.js
+++ project1/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const userModel = require('../models/user.model')
-const crypto = require('crypto')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 async function registerController(req,res){
@@ -16,7 +16,7 @@ async function registerController(req,res){
         })
     }
 
-    const hash = crypto.createHash('sha256').update(password).digest('hex')
+    const hash = await bcrypt.hash(password, 10)
 
     const user = await userModel.create({
         username,
@@ -63,12 +63,11 @@ async function loginController(req,res){
 
     if(!user){
         return res.status(404).json({
-            message:"user not founf"
+            message:"user not found"
         })
     }
-    const hash = crypto.createHash('sha256').update(password).digest('hex')
 
-    const isPasswordValid = hash == user.password
+    const isPasswordValid = await bcrypt.compare(password, user.password)
 
     if(!isPasswordValid){
         return res.status(401).json({
@@ -94,6 +93,8 @@ async function loginController(req,res){
         }
     })
 }
+
+
 
 module.exports = {
     registerController,
