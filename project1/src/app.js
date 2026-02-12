8745faf4b18f0184a3ce379c1985a7ca9f9a2493
sarva1210 <sarva.vish77@gmail.com>
2026-02-12 20:08:23 +0530

    js file

--- project1/src/app.js
+++ project1/src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser')
 
 const authRouter = require('./routes/auth.routes')
 const postRouter = require('./routes/post.routes')
+
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
