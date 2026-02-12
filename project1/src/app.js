bc79d71922e3146617f98a3c3aef9a2e88ebf09d
sarva1210 <sarva.vish77@gmail.com>
2026-02-12 20:08:39 +0530

    js file

--- project1/src/app.js
+++ project1/src/app.js
@@ -5,6 +5,7 @@ const authRouter = require('./routes/auth.routes')
 const postRouter = require('./routes/post.routes')
 
 const app = express()
+
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
