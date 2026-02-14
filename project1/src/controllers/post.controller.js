0e51102c3ae9f9ac2ecb569c59e315632be13dec
sarva1210 <sarva.vish77@gmail.com>
2026-02-14 22:19:01 +0530

    post  controller

--- project1/src/controllers/post.controller.js
+++ project1/src/controllers/post.controller.js
@@ -47,6 +47,70 @@ async function createPostController(req,res) {
     })
 }
 
+async function getPostController(req,res){
+    const token = req.cookies.token
+    if(!token){
+        return res.status(401).json({
+            message:"UnAuthorized Access"
+        })
+    }
+    let decoded;
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+            message:"Token invalid"
+        })
+    }
+    const userId = decoded.id
+    const posts = await postModel.find({
+        user:userId
+    })
+    res.status(200).json({
+        message:"Posts fetched successfully",
+        posts
+    })
+}
+async function getPostDetailsController(req,res) {
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message:"UnAuthorized Access"
+        })
+    }
+    let decoded
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+            message:"Invaild Token"
+        })
+    }
+    const userId = decoded.id
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+    if(!post){
+        return res.status(404).json({
+            message:"Post not found"
+        })
+    }
+    // const isValidUser = post.user == userId
+    const isValidUser = post.user.toString() === userId
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message:"Forbidden Content"
+        })
+    }
+    return res.status(200).json({
+        message:"Post fetched successfully",
+        posts
+    })
+}
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetailsController
 }
--- project1/src/routes/post.routes.js
+++ project1/src/routes/post.routes.js
@@ -7,5 +7,10 @@ const upload = multer({storage:multer.memoryStorage()})
 // post /api/posts {protected}
 postRouter.post("/",upload.single("image"), postController.createPostController)
 
+// get /api/posts
+postRouter.get("/",postController.getPostController)
+
+// get /api/posts/details/:postid
+postRouter.get("/details:postId", postController.getPostDetailsController)
 
 module.exports = postRouter
