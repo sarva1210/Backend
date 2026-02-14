7fb880aeefa8c0a2d900223dc2339e70d4f5bc2b
sarva1210 <sarva.vish77@gmail.com>
2026-02-14 22:24:19 +0530

    server js

--- project1/server.js
+++ project1/server.js
@@ -3,6 +3,7 @@ const app = require('./src/app')
 const connectToDatabase = require('./src/config/database')
 
 connectToDatabase()
+
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
 })
