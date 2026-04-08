8b5e8f0045a871d87348605b394054c4eb637591
sarva1210 <sarva.vish77@gmail.com>
2026-04-08 21:15:55 +0530

    integrating validator

--- Day10/src/validation/auth.validator.js
+++ Day10/src/validation/auth.validator.js
@@ -15,8 +15,9 @@ const validate = (req, res, next) => {
 
 export const registerValidation = [
     body("username").isString().withMessage("username should be string"),
-    
+
     body("email").isEmail().withMessage("email should be valid email address"),
+    
     body("password").custom((value) => {
         if (value.length < 6) {
             throw new Error("password should be at least 6 characters long")
